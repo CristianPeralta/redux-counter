063bfc3e5a484bd5fe550b9e4ff587893e0a96df
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-06 09:01:39 -0500

    use updateObject utility into result reducer

--- src/store/reducer/result.js
+++ src/store/reducer/result.js
@@ -12,16 +12,10 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case STORE_RESULT:{
-            return {
-                ...state,
-                results: state.results.concat({ id: new Date(), value: action.result }),
-            };
+            return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result }) });
         }
         case DELETE_RESULT:{
-            return {
-                ...state,
-                results: state.results.filter(result => result.id !== action.id),
-            };
+            return updateObject(state, { results: state.results.filter(result => result.id !== action.id) });
         }
     }
     return state;
