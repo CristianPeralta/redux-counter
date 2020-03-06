604b574db9371a0774531ff8d68239c7dd5d084d
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-06 09:20:44 -0500

    add deleteResult function

--- src/store/reducer/result.js
+++ src/store/reducer/result.js
@@ -9,14 +9,15 @@ const initialState = {
     results: []
 };
 
+const deleteResult = (state, action) => {
+    const filteredResults = state.results.filter(result => result.id !== action.id)
+    return updateObject(state, { results: filteredResults });
+}
+
 const reducer = (state = initialState, action) => {
     switch (action.type) {
-        case STORE_RESULT:{
-            return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result }) });
-        }
-        case DELETE_RESULT:{
-            return updateObject(state, { results: state.results.filter(result => result.id !== action.id) });
-        }
+        case STORE_RESULT : return updateObject(state, { results: state.results.concat({ id: new Date(), value: action.result }) });
+        case DELETE_RESULT : return deleteResult(state, action);
     }
     return state;
 };
